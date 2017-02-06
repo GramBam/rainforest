9fa9a5d746fa90110d7a2fb2188df3bb3cf05c9a
Graham Moss <graham.moss@hotmail.com>
2017-02-06 17:28:34 -0500

    fixed errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :products, through: :reviews
   has_secure_password
-  validates: :name, presence: true
+  validates :name, presence: true
 end
